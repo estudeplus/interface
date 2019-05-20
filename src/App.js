52d27110bc0f7505e0064389cadb05e138cd6291
ericGOD24 <ericobandeira@hotmail.com>
2019-05-19 22:34:01 -0300

    feat(#2): fix problem with pre-cadastro
    Co-authored-by: max <maxhb.df@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import Precadastro from './components/pre-cadastro';
+import Precadastro from './components/Pre-cadastro/pre-cadastro';
+import 'materialize-css/dist/css/materialize.min.css'
 
 function App() {
   return (
     <div>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
       <Precadastro/>
     </div>
   );
---
+++ src/components/Pre-cadastro/pre-cadastro.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import '../../stylesheet/Pre-cadastro/pre-cadastro.css'
+
+class Precadastro extends Component {
+  render() {
+    return (
+    <div>
+      <div className="nav">
+        {/* <img src={require('../../img/Estude+ sem fundo.png')} alt="logo"></img> */}
+      </div>
+      
+
+      <div class="card box1">
+          <br></br>
+          <h2 className="texto">Pré-cadastro</h2>
+          <br></br>
+          <br></br>
+          <div class="card-content">
+                <div class="row">
+            <div class="input-field col s12">
+              <input id="icon_prefix" type="text" class="validate"/>
+              <label for="icon_prefix"></label>
+              <label class="active" for="first_name2">Matrícula</label>
+            </div>
+          </div>     
+        </div>
+        <div class="card-tabs">
+          <ul class="tabs tabs-fixed-width">
+            <li class="tab">
+              <button class="btn waves-effect waves-light" type="submit" name="action">Registrar
+              </button>
+            </li>
+            <li class="tab">
+              <button class="btn waves-effect waves-light" type="submit" name="action">Login
+                <i class="material-icons right">send</i>
+              </button>  
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>  
+    )
+  }
+}
+export default Precadastro
